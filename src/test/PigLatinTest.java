a084b7528dec4c5b5e63dccd91af56fc31ccba96
Victor Millnert <victor@control.lth.se>
2017-03-08 15:05:31 +0100

    added a dictionary of words to test the translation

--- src/test/PigLatinTest.java
+++ src/test/PigLatinTest.java
@@ -1,11 +1,29 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Collections;
+
 public class PigLatinTest {
-  @Test
-  public void translateWord() {
-    PigLatin pig = new PigLatin();
-    String word = pig.translate("pig");
-    assertEquals("igpay", word);
-  }
+        @Test
+        public void translateWord() {
+                PigLatin pig = new PigLatin();
+                Map<String, String> map = new HashMap<String, String>();
+
+                map.put("pig", "igpay");
+                map.put("latin", "atinlay");
+                map.put("banana", "ananabay");
+                map.put("trash", "ashtray");
+                
+                Iterator it = map.entrySet().iterator();
+                while (it.hasNext()){
+                        Map.Entry pair = (Map.Entry)it.next();
+                        String correct = (String)pair.getValue();
+                        String translated = pig.translate((String)pair.getKey());
+                        assertEquals(correct, translated);
+                }
+        }
+
 }
